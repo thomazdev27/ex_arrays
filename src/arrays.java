e6491542874881b83071db6125ffd00d90780970
david thomaz teixeira dos santos <123262068+thomazdev27@users.noreply.github.com>
2023-03-29 17:33:28 -0300

    exercícios Práticos de arrays.java

---
+++ src/arrays.java
@@ -0,0 +1,19 @@
+public class arrays {
+    public static void main(String[] args) {
+
+        int[] vetor = new int[]{-5, -6, 15, 50, 8, 4};
+        System.out.print("Vetor: ");
+
+        for (int j : vetor) {
+            System.out.print(j + " ");
+        }
+
+        System.out.print("\nVetor: ");
+
+        for(int i = vetor.length - 1; i >= 0; --i) {
+            System.out.print(vetor[i] + " ");
+        }
+
+    }
+
+    }
